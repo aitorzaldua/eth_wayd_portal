e436431dd0fbecd26229a6134b8987fc50a72b5d
Aitor Zaldua <aitor.zaldua@draftdigital.org>
2022-01-21 20:54:12 +0100

    jan 21 3

--- src/App.js
+++ src/App.js
@@ -17,18 +17,17 @@ function App() {
   //Html:
   return (
     <div className="App">
-      <header className="App-header">
-        {/* <img src= {logo} className="App-logo" alt="App-logo" /> */}
+      <header className="Header">
         <p>This is the Header</p>
-        <a className="App-link"
-          href="https://draftdigital.org">
-          Draft Digital
-        </a>
+        <a className="App-link" href="https://draftdigital.org"> Draft Digital</a>
       </header>
-      <body>
+      <body className="Body">
         <p>This is the body</p>
+        <button className="waveButton" onClick='null'>
+          Click the button
+        </button>
       </body>
-      <footer>
+      <footer className="Footer">>
         <p>This is the footer</p>
       </footer>
     </div>
