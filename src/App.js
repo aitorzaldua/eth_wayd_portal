3e382bdee161afff4621a851a82fef40713ea229
Aitor Zaldua <aitor.zaldua@draftdigital.org>
2022-01-21 20:08:28 +0100

    jan 21 3

--- src/App.js
+++ src/App.js
@@ -1,23 +1,36 @@
-import logo from './logo.svg';
+/* import logo from './logo.svg'; */
 import './App.css';
 
 function App() {
+
+
+
+
+
+
+
+
+
+
+
+
+  //Html:
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
+        {/* <img src= {logo} className="App-logo" alt="App-logo" /> */}
+        <p>This is the Header</p>
+        <a className="App-link"
+          href="https://draftdigital.org">
+          Draft Digital
         </a>
       </header>
+      <body>
+        <p>This is the body</p>
+      </body>
+      <footer>
+        <p>This is the footer</p>
+      </footer>
     </div>
   );
 }
